0503ec1f464ba4d031b0b750f63c1ed26d712cfc
lalit-003 <gahlawatl7@gmail.com>
2020-11-30 07:14:22 +0530

    Added functionality to create a spring project and demonstrate REST API calls and CURD operations

---
+++ EmployeePayrollSpring/src/test/java/com/bridgelabz/EmployeePayrollObject/EmployeePayrollObjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.EmployeePayrollObject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePayrollObjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
